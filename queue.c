572e86123d5d6d75c792853b3403c785f294766a
egibilis <egibilis@purdue.edu>
2025-04-03 23:28:07 -0400

    Update queue.c

--- queue.c
+++ queue.c
@@ -1,8 +1,53 @@
 #include "queue.h"
 #include "tile_game.h"
 
-void enqueue(struct queue *q, struct game_state state) {}
+//void enqueue(struct queue *q, struct game_state state) {}
+void enqueue(struct queue *q, struct game_state state){
+    size_t encoded = serialize(state);
+    insert_at_tail(&(q->list), encoded);
 
-struct game_state dequeue(struct queue *q) { return (struct game_state){0}; }
+}
 
-int number_of_moves(struct game_state start) { return 0; }
+//struct game_state dequeue(struct queue *q) { return (struct game_state){0}; }
+struct game_state dequeue(struct queue *q){
+    size_t encoded = remove_from_head(&(q->list));
+    return deserialize(encoded);
+}
+
+//int number_of_moves(struct game_state start) { return 0; }
+int number_of_moves(struct game_state start){
+    if (is_solved(start)) return 0;
+
+    struct queue q = {0};  //queue of states
+    enqueue(&q, start);
+
+    struct hash_table visited = {0};
+    hash_table_init(&visited);
+
+    while(q.list.head != NULL){
+        struct game_state curr = dequeue(&q);
+        size_t curr_encoded = serialize(curr);
+
+        if (hash_table_contains(&visited, curr_encoded)){
+            continue;
+        }
+        hash_table_insert(&visited,curr_encoded);
+        struct game_state children[4];
+        int num = generate_all_moves(curr,children);
+
+        for (int i = 0; i < num; i++){
+            struct game_state next = children[i];
+            if(is_solved(next)){
+                free_list(q.list);
+                hash_table_destroy(&visited);
+                return next.moves;
+            }
+            enqueue(&q,next);
+        }
+    }
+    free_list(q.list);
+    hash_table_destroy(&visited);
+    return -1;//unsolveble chance
+
+}
+
