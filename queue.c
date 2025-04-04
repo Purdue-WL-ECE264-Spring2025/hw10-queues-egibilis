2f7703bdc1945b20455391db47ccacbc0a1239c8
egibilis <egibilis@purdue.edu>
2025-04-04 13:44:57 -0400

    Update queue.c

--- queue.c
+++ queue.c
@@ -4,18 +4,19 @@
 //void enqueue(struct queue *q, struct game_state state) {}
 void enqueue(struct queue *q, struct game_state state){
     size_t encoded = serialize(state);
-    insert_at_tail(&(q->data), encoded);
+    insert_at_tail(&q->data, encoded);
 
 }
 
 //struct game_state dequeue(struct queue *q) { return (struct game_state){0}; }
 struct game_state dequeue(struct queue *q){
-    size_t encoded = remove_from_head(&(q->data));
+    size_t encoded = remove_from_head(&q->data);
     return deserialize(encoded);
 }
 
 //int number_of_moves(struct game_state start) { return 0; }
 int number_of_moves(struct game_state start){
+    /*
     if (is_solved(start)) return 0;
 
     struct queue q = {0};  //queue of states
@@ -47,6 +48,7 @@ int number_of_moves(struct game_state start){
     }
     free_list(q.data);
     hash_table_destroy(&visited);
+    */
     return -1;//unsolveble chance
 
 }
